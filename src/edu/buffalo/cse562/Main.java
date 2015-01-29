f8dd1a1a9c662b72ee5423b06422eff65b9703d5
Arindam Nandi <arindamn@buffalo.edu>
2015-01-28 20:51:59 -0500

    Checkpoint 0

---
+++ src/edu/buffalo/cse562/Main.java
@@ -0,0 +1,12 @@
+package edu.buffalo.cse562;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("We, the members of our team, agree that we will " +
+				"not submit any code that we have not written ourselves, " +
+				"share our code with anyone outside of our group, or use " +
+				"code that we have not written ourselves as a reference.");
+	}
+
+}
