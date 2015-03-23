3fd78d50b56b6979103cd26489f3fd3c6df391bd
Saptarshi Bhattacharjee <saptarsh@buffalo.edu>
2015-03-22 22:07:35 -0400

    Fixed opttimize parameter

--- src/edu/buffalo/cse562/ParseTreeOptimizer.java
+++ src/edu/buffalo/cse562/ParseTreeOptimizer.java
@@ -161,7 +161,7 @@ public class ParseTreeOptimizer {
 		}
 		
 		if(parseTree instanceof OrderByOperator) {
-		parseTree = new ExternalSortOperator((OrderByOperator) parseTree, 2);
+		parseTree = new ExternalSortOperator((OrderByOperator) parseTree, 10000);
 		}
 		
 		
