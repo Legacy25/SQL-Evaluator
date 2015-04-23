b5b2903f6ccec9c85f41e49417f6a1a7a7f9c9d9
Arindam Nandi <arindamnandidbl@gmail.com>
2015-04-23 01:11:03 -0400

    Reverting to 9.75

--- sql/tpch10.sql
+++ sql/tpch10.sql
@@ -85,27 +85,12 @@ CREATE TABLE REGION (
     );
 
 SELECT  
-        customer.custkey, 
-        customer.name, 
-        customer.acctbal,
-        nation.name,
-        customer.address,
-        customer.phone,
-        customer.comment,
-        SUM(lineitem.extendedprice * (1 - lineitem.discount)) AS revenue
+        orders.orderkey,
+        orders.orderdate
 FROM    
-        customer, 
-        orders,
-        lineitem,
-        nation
+        orders
 WHERE   
-        customer.custkey = orders.custkey
-        AND   lineitem.orderkey = orders.orderkey
-        AND   orders.orderdate >= DATE('1993-10-01')
+        orders.orderdate >= DATE('1993-10-01')
         AND   orders.orderdate < DATE('1994-01-01')
-        AND   lineitem.returnflag = 'R'
-        AND   customer.nationkey = nation.nationkey
-GROUP BY 
-        customer.custkey, customer.name, customer.acctbal, customer.phone, nation.name, customer.address, customer.comment;
-
-customer, orders, lineitem, nation
+ORDER BY
+        orders.orderdate desc;
