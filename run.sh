420a2f521edddfa85443179a16a70298261a46b4
Arindam Nandi <arindamnandidbl@gmail.com>
2015-09-19 20:42:25 -0400

    Runtime flags in run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd $SCRIPTPATH
-java -classpath bin:"lib/*" edu.buffalo.cse562.Main "$@"
+java -XX:+UseCompressedOops -Xms512m -Xmx2048m -classpath bin:"lib/*" edu.buffalo.cse562.Main "$@"
