3b1b2612b194f2575cfd2812f0c9f78208e95322
Arindam Nandi <arindamnandidbl@gmail.com>
2015-09-19 20:24:29 -0400

    Added --time flag

--- src/edu/buffalo/cse562/Main.java
+++ src/edu/buffalo/cse562/Main.java
@@ -9,7 +9,9 @@
 
 package edu.buffalo.cse562;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
@@ -17,6 +19,8 @@ import java.util.ArrayList;
 import edu.buffalo.cse562.operators.Operator;
 
 public class Main {	
+	
+	public static final int BILLION = 1000*1000*1000;
 	
 	/* 
 	 * Stores the swap directory
@@ -60,6 +64,13 @@ public class Main {
 	public static File FILE_OUTPUT_DIR = null;
 	
 	/*
+	 * --time flag prints out query execution times to a file
+	 */
+	public static boolean TIME_OUTPUT = false;
+	public static File TIME_OUTPUT_DIR = null;
+	
+	
+	/*
 	 * -q Quiet mode
 	 */
 	public static boolean QUIET = false;
@@ -73,6 +84,12 @@ public class Main {
 		
 		/* Stores the SQL files */
 		ArrayList<File> sqlFiles = new ArrayList<File>();
+		
+		/* Stores query generation times for each query */
+		ArrayList<Double> qgenTime = new ArrayList<Double>();
+		
+		/* Stores query execution times for each query */
+		ArrayList<Double> qexecTime = new ArrayList<Double>();
 		
 		/*
 		 * CLI argument parsing
@@ -93,6 +110,11 @@ public class Main {
 			else if(args[i].equalsIgnoreCase("--fileout")) {
 				FILE_OUTPUT = true;
 				FILE_OUTPUT_DIR = new File(args[i+1]);
+				i++;
+			}
+			else if(args[i].equalsIgnoreCase("--time")) {
+				TIME_OUTPUT = true;
+				TIME_OUTPUT_DIR = new File(args[i+1]);
 				i++;
 			}
 			else if(args[i].equals("-q")) {
@@ -119,32 +141,42 @@ public class Main {
 		
 		/* 
 		 * Keep track of query time locally.
-		 * This code should be commented out before
-		 * commits for submissions.
 		 */
-		long start = System.nanoTime();
+		long globalStart = System.nanoTime();
 		
 		/* Generate the RA AST for each query */
 		ArrayList<Operator> parseTreeList = new ArrayList<Operator>();
 		for(int i = 0; i < sqlFiles.size(); i++) {
 			File f = sqlFiles.get(i);
+			long localStart = System.nanoTime();
 			parseTreeList.add(ParseTreeGenerator.generate(dataDirs, f));
+			
+			/* Compute the generation time for this query */
+			qgenTime.add((double) (System.nanoTime() - localStart)/BILLION);
 		}
 		
 		/* Optimize each AST */
 		for(int i = 0; i < parseTreeList.size(); i++) {
 			Operator parseTree = parseTreeList.get(i);
+			long localStart = System.nanoTime();
 			parseTreeList.set(i, ParseTreeOptimizer.optimize(parseTree));
+
+			/* Compute the optimization time for this query
+			 * and add it to the generation time 
+			 */
+			qgenTime.set(i, qgenTime.get(i) + (double) (System.nanoTime() - localStart)/BILLION);
 		}
 		
 		/*
 		 * Optimized query plans ready
 		 */
 		
-		long generateTime = System.nanoTime();
+		long totalGenerateTime = System.nanoTime();
 		
 		/* Now evaluate each parse-tree */
 		for(int i=0; i< parseTreeList.size(); i++) {
+			long localStart = System.nanoTime();
+			
 			if(parseTreeList.get(i) != null) {
 				if(FILE_OUTPUT) {
 					ParseTreeEvaluator.output(
@@ -158,20 +190,49 @@ public class Main {
 					ParseTreeEvaluator.output(parseTreeList.get(i));
 				}
 			}
+			
+			qexecTime.add((double) (System.nanoTime() - localStart)/BILLION);
 		}
+		
+		double totalGenTime = (double) (totalGenerateTime - globalStart)/BILLION;
+		double totalExecTime = (double) (System.nanoTime() - totalGenerateTime)/BILLION;
 		
 		/* DEBUG */
 		/* Show query times */
 		if(Main.DEBUG) {
 			
-			System.err.println("GENERATE TIME: "
-					+ ((double) (generateTime - start)/1000000000) 
+			System.err.println("TOTAL GENERATE TIME: "
+					+ (totalGenTime) 
 					+ "s");
 			
-			System.err.println("QUERY TIME: "
-					+ ((double) (System.nanoTime() - generateTime)/1000000000)
+			System.err.println("TOTAL EXECUTION TIME: "
+					+ (totalExecTime)
 					+ "s");
 		
 		}
+		
+		if(Main.TIME_OUTPUT) {
+			for(int i = 0; i < sqlFiles.size(); i++) {
+				try {
+					if(!FILE_OUTPUT_DIR.exists()) {
+						FILE_OUTPUT_DIR.mkdirs();
+					}
+					BufferedWriter bw = new BufferedWriter(
+							new FileWriter(
+									new File(
+											FILE_OUTPUT_DIR,
+											sqlFiles.get(i).getName().split(".sql")[0]+".stat")
+									)
+							);
+					
+					bw.write(qgenTime.get(i)+"|"+qexecTime.get(i));
+					bw.flush();
+					bw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
+
